8e3cb0d54f84d677ebec7e7e603e90f0480356d9
Nghia, Bui Gia <nghiabuigia2006@gmail.com>
2026-03-19 13:45:48 +0700

    Rewrite HustToken to better align to ERC20.

--- contracts/HustToken.sol
+++ contracts/HustToken.sol
@@ -1,38 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract HustToken {
-  string _name = "HustToken";
-  string _symbol = "HTN";
-  uint64 _decimals = 1 ether;
-  uint256 _totalSupply = 100000 ether;
-  uint256 _currentSupply = 0;
+import "./IERC20.sol";
+import "./SafeMath.sol";
 
+contract HustToken is IERC20 {
+  address private owner;
+  uint256 _totalSupply = 10000 wei;
+  uint256 _currentSupply = 0;
   mapping (address => uint256) balances;
   mapping (address => mapping(address => uint256)) allowances;
 
-  event Transfer (
-    address indexed sender,
-    address indexed receiver,
-    uint256 amount
-  );
-
-  event Approval (
-    address indexed _owner,
-    address indexed _spender,
-    uint256 _value
-  );
-
-  function name() public view returns (string memory) {
-    return _name;
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
   }
 
-  function symbol() public view returns (string memory) {
-    return _symbol;
+  function name() public view returns (string) {
+    return "HUSTToken";
   }
 
-  function decimals() public view returns (uint256) {
-    return _decimals;
+  function symbol() public view returns (string) {
+    return "HUST";
+  }
+
+  function decimals() public view returns (uint8) {
+    return 18; // Just like wei. 
   }
 
   function totalSupply() public view returns (uint256) {
@@ -47,34 +40,55 @@ contract HustToken {
     return allowances[_owner][_spender];
   }
 
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    require(_to != address(0));
+    return _transfer(msg.sender, _to, _value);
+
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    require((_from != address(0)) || (_to != address(0)));
+    return _transfer(msg.sender, _to, _value);
+  }
+
+  function mint(address _to, uint256 _value) onlyOwner returns (bool success) {
+    require(_to != address(0));
+    return _transfer(address(0), _to, _value);
+  }
+
+  function burn(uint256 _value) public returns (bool success) {
+    require(balances[msg.sender] >= _value);
+    return _transfer(msg.sender, address(0), _value);
+  }
+
   function approve(address _spender, uint256 _value) public returns (bool success) {
+    require(_spender != address(0));
+    require(balances[msg.sender] >= _value); // Avoid overspend
     allowances[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
     return true;
   }
 
-  function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balances[msg.sender] >= _value, "Not enough token to complete the transaction!");
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    emit Transfer(msg.sender, _to, _value);
-    return true;
-  }
-
-  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-    require(balances[_from] >= _value, "Not enough token to complete the transaction!");
-    require(allowances[_from][_to] >= _value, "The owner allows less token than the transaction!");
-    balances[_from] -= _value;
-    balances[_to] -= _value;
-    allowances[_from][_to] -= _value;
+  function _transfer(address _from, address _to, uint256 _value) returns (bool success) {
+    if (_from == address(0)) {
+      // Minting new token.
+      require(_currentSupply + _value <= _totalSupply);
+      balances[_to] += _value;
+      _currentSupply += _value;
+    }
+    else if (_to == address(0)) {
+      // Burning token
+      require(balances[_from] >= _value);
+      balances[_from] -= _value;
+      _currentSupply -= _value;
+    }
+    else {
+      require((balances[_from] >= _value) || (allowances[_from][_to] >= _value));
+      balances[_from] -= _value;
+      balances[_to] += _value;
+      allowances[_from][_to] -= _value;
+    }
     emit Transfer(_from, _to, _value);
-    return true;
-  }
-
-  function mint(address _minter, uint256 _value) public returns (bool success) {
-    require(_currentSupply + _value <= _totalSupply, "Current supply overflows total allowed supply");
-    balances[_minter] += _value;
-    _currentSupply += _value;
     return true;
   }
 }
